366789bf61b35ae1c687f2ea1500b4ead6a2e810
lubson <l.hruban@gmail.com>
2015-05-31 23:39:36 +0200

    Issue with tests implemented

---
+++ hours_in_each_state/lib/issue.rb
@@ -0,0 +1,38 @@
+class Issue
+  attr_writer :body, :histories
+
+  def initialize(body)
+    @body = body
+  end
+
+  def get_key
+    return @body["key"]
+  end
+
+  def get_summary
+    @body["fields"]["summary"]
+  end
+
+  def get_type
+    return @body["fields"]["issuetype"]["name"]
+  end
+
+  def get_transitions
+    transitions = []
+    @histories.each do |history|
+       if (history["items"][0]["field"] == "status")
+         transition = [
+           history["items"][0]["fromString"], 
+           history["items"][0]["toString"], 
+           history["created"]
+         ]
+         transitions << transition
+       end
+    end   
+    return transitions
+  end
+
+  def ooc?
+    return get_summary.include? "OOC"
+  end
+end
---
+++ hours_in_each_state/spec/lib/issue_spec.rb
@@ -0,0 +1,85 @@
+require 'issue'
+require 'json'
+
+describe "An Issue" do
+  
+  son_75 = Issue.new(
+    JSON.parse(
+      '{
+        "key":"SON-75", 
+        "fields":{
+          "issuetype":{
+            "name":"Story"
+          },
+          "summary":"Setup JBOSS 7"
+        }
+      }'
+    )
+  )
+ 
+  son_75.histories = 
+	JSON.parse(   
+
+	    '[
+	        {
+	          "created":"2015-03-12T09:22:46.262-0500", 
+	          "items":[{
+	      	    "field":"status", 
+	      	    "fromString":"Defined", 
+	      	    "toString":"Selected"
+	          }]
+	        },{
+	          "created":"2015-03-13T09:22:46.262-0500", 
+	          "items":[{
+	      	    "field":"status", 
+	      	    "fromString":"Selected", 
+	      	    "toString":"Investigation In-Progress"
+	          }]
+	        },{
+	          "created":"2015-03-15T09:22:46.262-0500", 
+	          "items":[{
+	      	    "field":"status", 
+	      	    "fromString":"Investigation In-Progress", 
+	      	    "toString":"Investigation Done"
+	          }]
+	        },{
+	          "created":"2015-03-19T09:22:46.262-0500", 
+	          "items":[{
+	      	    "field":"status", 
+	      	    "fromString":"Investigation Done", 
+	      	    "toString":"Accepted"
+	          }]
+	        },{
+	          "created":"2015-03-19T09:22:46.262-0500", 
+	          "items":[{
+	      	    "field":"workflow", 
+	      	    "fromString":"Agile Simplified Workflow for Project SON", 
+	      	    "toString":"Software Development Workflow"
+	          }]
+	        }
+	      ]'
+	)
+
+  it "has key SON-75" do
+    expect(son_75.get_key).to be == "SON-75"
+  end
+
+  it "is not OOC" do
+    expect(son_75).not_to be_ooc
+  end
+
+  it "is type of Story" do
+  	expect(son_75.get_type).to be == "Story"
+  end
+
+  it "has right summary" do
+  	expect(son_75.get_summary).to be == "Setup JBOSS 7"
+  end
+
+  it "has apropriate transitions" do
+    transitions = son_75.get_transitions
+    expect(transitions.count).to be == 4
+    expect(transitions.last).to be == ["Investigation Done","Accepted","2015-03-19T09:22:46.262-0500"]
+  end
+  
+end
