9e2bc18d682f4ca8dec44a7235195fd07189b5f9
lubson <l.hruban@gmail.com>
2015-06-02 18:57:37 +0000

    Generating hours spend in transition instead of timestamp

--- hours_in_each_state/scripts/print_sprint_transitions_as_csv.rb
+++ hours_in_each_state/scripts/print_sprint_transitions_as_csv.rb
@@ -3,12 +3,21 @@ require_relative '../lib/issue_manager'
 #add argument for sprint and date, store file normally and use it as input for non existing R sript :)
 manager = IssueManager.new
 
-issues = manager.get_finished_issues_in_sprint(54,'2015-05-28')
+issues = manager.get_finished_issues_in_sprint(31,'2015-05-28')
 
 puts "Key, Date, From, To"
 
 issues.each do |issue|
   manager.set_changelog!(issue)
   transitions = issue.get_transitions
-  transitions.each {|transition| puts "#{issue.get_key},#{transition[2]},#{transition[0]},#{transition[1]}"} 
+  previous_transition = nil
+  transitions.each do |transition|
+    if previous_transition.nil? 
+        hours = 0  
+    else
+        hours = ((DateTime.parse(transition[2].to_s) - DateTime.parse(previous_transition.to_s))*24 *60).to_i/60.0
+    end
+    puts "#{issue.get_key},#{hours},#{transition[0]},#{transition[1]}"
+    previous_transition = transition
+  end
 end
